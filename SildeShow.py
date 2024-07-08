2fb68f0b6577f4a5453744d1a9497149e30a5745
best01211 <jinwoobest.andy01@gmail.com>
2024-07-08 17:29:08 +0900

    Update SildeShow.py

--- SildeShow.py
+++ SildeShow.py
@@ -11,3 +11,23 @@ if not img_files:
 
 for f in img_files:
     print(f)
+
+# 슬라이드쇼를 표시
+cv2.namedWindow('image', cv2.WINDOW_NORMAL)
+cv2.setWindowProperty('image', cv2.WND_PROP_FULLSCREEN, cv2.WINDOW_FULLSCREEN)
+
+cnt = len(img_files)
+idx=0
+
+while True:
+    img = cv2.imread(img_files[idx])
+
+    cv2.imshow('image', img)
+
+    if cv2.waitKey(1000) == 27:
+        break
+    
+    idx +=1
+    if idx >= cnt:
+        idx = 0
+cv2.destroyAllWindows
